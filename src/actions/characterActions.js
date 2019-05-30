27918d2ea6b47ce11fab3dc30f03ad65be3b42e7
Parker Helland <hellandp@gmail.com>
2019-05-30 14:04:19 -0700

    refactor using promise middleware

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,12 +1,7 @@
 import { getCharactersApi } from '../services/avatarApi';
 
 export const GET_CHARACTERS = 'GET_CHARACTERS';
-export const characterActions = () => dispatch => {
-  return getCharactersApi()
-    .then(results => {
-      dispatch({
-        type: GET_CHARACTERS,
-        payload: results
-      });
-    });
-};
+export const characterActions = () => ({
+  type: GET_CHARACTERS,
+  payload: getCharactersApi()
+}); 
--- src/store.js
+++ src/store.js
@@ -3,13 +3,13 @@ import {
   compose,
   createStore
 } from 'redux';
-import thunk from 'redux-thunk';
+import { promiseMiddleware } from 'promise-middleware-redux';
 import reducers from './reducers';
 
 export default createStore(
   reducers,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(promiseMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
