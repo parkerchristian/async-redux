e2188a4aa5c25b814c6cafa7b40eed99e9e49143
Parker Helland <hellandp@gmail.com>
2019-05-30 11:15:05 -0700

    import default photo

--- src/services/avatarApi.js
+++ src/services/avatarApi.js
@@ -1,9 +1,12 @@
+import image from '../assets/avatarAirbender.jpg';
+const photo = image;
+
 export const getCharactersApi = () => {
   return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
     .then(res => res.json())
     .then(res => res.map(character => ({
       id: character._id,
-      photoUrl: character.photoUrl,
+      photoUrl: character.photoUrl || photo,
       name: character.name
     })));
 };
