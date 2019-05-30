b0ea4bfa0b4fef2ef1d14ca1dca097c0cb28b11c
Parker Helland <hellandp@gmail.com>
2019-05-30 14:20:27 -0700

    css

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -11,13 +11,15 @@ function Character({ character }) {
     alignItems: 'center',
     margin: '5px',
     border: 'solid',
-    borderRadius: '100%'
+    borderRadius: '100%',
+    boxShadow: '5px -2px 8px black'
   };
   return (
     <section style={css}>
-      <img src={character.photoUrl} style={{ width: '150px', height: '150px',
+      <img src={character.photoUrl} style={{ boxShadow: '5px -2px 5px #888888', width: '150px', height: '150px',
         borderRadius: '100%' }}/>
-      <p style={{ position: 'absolute', marginTop: '75px', background: 'white' }}>{character.name}</p>
+      <p style={{ borderRadius: '50%', padding: '5px', position: 'absolute', marginTop: '75px', background: 'white',
+        opacity: '1' }}>{character.name}</p>
     </section>
   );
 }
